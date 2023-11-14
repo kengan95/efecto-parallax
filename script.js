a32620dc07fd8b7106ffb47ce26742cdd0b7916d
ramiroSicilia <moto2.belgrano@gmail.com>
2023-11-14 20:18:27 -0300

    nuevo parallax

---
+++ script.js
@@ -0,0 +1,58 @@
+/*let scene=document.getElementById('scene');
+let text = document.getElementById('text');
+let parallaxInstanceScene= new Parallax(scene);
+let parallaxInstanceText= new Parallax(text);
+parallaxInstanceScene.friction(0.2, 0.2);*/ 
+
+
+let escena=document.getElementById('scene')
+let texto=document.getElementById('text')
+let efectoScene=new Parallax(escena)
+let efectoText=new Parallax(texto) 
+efectoScene.friction(0.2,0.2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
